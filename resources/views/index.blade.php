ab2960043af17ab4d9be1a83753da0380e9d56d2
Lucas <lucas.lubasinski11@gmail.com>
2021-02-21 17:28:37 +0100

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+
+@extends('layouts.default')
+@section('content')
+@include('partials.navbar')
+
+<div class="img-index">
+    <div class="text-index">
+        <h2>Réservez maintenant!</h2>
+        <a href="/reservation">
+        <button class="p-2 my-2 bg-blue-500 text-white rounded-md focus:outline-none focus:ring-2 ring-blue-300 ring-offset-2">Réserver</button>
+        </a>
+        
+    </div>
+</div>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,12 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<div class="relative bg-white navbar">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6">
+    <div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+      <div class="flex justify-start lg:w-0 lg:flex-1">
+        <a href="/">
+          <h1>Affluence</h1>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
